577fa4ba478f656a3829ca56d769977328007086
Abdullah Hamada <ab989b25@gmail.com>
2023-11-10 17:51:03 +0300

    refactor migration files

--- database/migrations/2023_11_04_164317_create_jobs_table.php
+++ database/migrations/2023_11_04_164317_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobListingsTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateJobListingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('employer_id');
             $table->string('title');
@@ -38,6 +38,6 @@ class CreateJobListingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('jobs');
     }
 }
--- database/migrations/2023_11_04_164325_create_applications_table.php
+++ database/migrations/2023_11_04_164325_create_applications_table.php
@@ -15,13 +15,13 @@ class CreateApplicationsTable extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('job_listing_id');
+            $table->unsignedBigInteger('job_id');
             $table->unsignedBigInteger('user_id');
             $table->text('cover_letter');
             $table->timestamps();
             $table->softDeletes();
 
-            $table->foreign('job_listing_id')->references('id')->on('job_listings')->onDelete('cascade');
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
