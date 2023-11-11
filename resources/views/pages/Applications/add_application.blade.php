e2e7b509f0e6310f6f9e25388c3e91965e19848d
Abdullah Hamada <ab989b25@gmail.com>
2023-11-11 18:00:17 +0300

    edit add aplication blade

--- resources/views/pages/Applications/add_application.blade.php
+++ resources/views/pages/Applications/add_application.blade.php
@@ -2,13 +2,13 @@
 @section('css')
     @toastr_css
 @section('title')
-    {{trans('Jobs_trans.add_Job')}}
+    {{trans('Applications_trans.apply_for_Job')}}
 @stop
 @endsection
 @section('page-header')
 <!-- breadcrumb -->
 @section('PageTitle')
-    {{trans('Jobs_trans.add_Job')}}
+    {{trans('Applications_trans.apply_for_Job')}}
 @stop
 
 
@@ -16,93 +16,98 @@
 <!-- breadcrumb -->
 @endsection
 @section('content')
-<!-- row -->
-<div class="row">
-    <div class="col-md-12 mb-30">
-        <div class="card card-statistics h-100">
-            <div class="card-body">
-
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <form method="post"  action="{{ route('Jobs.store') }}" autocomplete="off" enctype="multipart/form-data">
-                    @csrf
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mb-30">
+            <div class="card card-statistics h-100">
+                <div class="card-body">
                     <h6 style="font-family: 'Cairo', sans-serif;color: blue">{{trans('Jobs_trans.jobs_information')}}</h6><br>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.title')}} : <span class="text-danger">*</span></label>
-                                    <input  type="text" name="title"  class="form-control">
-                                </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.title')}}</label>
+                                <input value="{{$Job->title}}"  disabled type="text" name="title"  class="form-control">
                             </div>
                         </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.description')}} : </label>
-                                    <input type="text"  name="description" class="form-control" >
-                                </div>
-                            </div>
-
-
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.location')}} : </label>
-                                    <input type="text"  name="location" class="form-control" >
-                                </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.location')}}</label>
+                                <input value="{{$Job->location}}" disabled type="text" name="location"  class="form-control">
                             </div>
                         </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.salary_range')}} : <span class="text-danger">*</span></label>
-                                    <input  type="text" name="salary_range"  class="form-control">
-                                </div>
-                            </div>
-
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.employment_type')}} : <span class="text-danger">*</span></label>
-                                    <select name="employment_type" class="form-control">
-                                        <option value="#">الرجاء الاختيار</option>
-                                        <option value="full-time">Full-time</option>
-                                        <option value="part-time">Part-time</option>
-                                        <option value="Contract">Contract</option>
-                                    </select>
-                                </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.salary_range')}}</label>
+                                <input value="{{$Job->salary_range}}" disabled type="text" name="salary_range"  class="form-control">
                             </div>
                         </div>
-
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label>{{trans('Jobs_trans.status')}} : <span class="text-danger">*</span></label>
-                                    <select name="status" class="form-control">
-                                        <option value="#">الرجاء الاختيار</option>
-                                        <option value="open">Open</option>
-                                        <option value="closed">Closed</option>
-                                    </select>
-                                </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.employment_type')}}</label>
+                                <input value="{{$Job->employment_type}}" disabled type="text" name="employment_type"  class="form-control">
                             </div>
                         </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.posted_at')}}</label>
+                                <input value="{{ \Carbon\Carbon::parse($Job->posted_at)->diffForHumans() }}" style="color: blue" type="text" name="expires_at" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{trans('Jobs_trans.expires_at')}}</label>
+                                <input value="{{$Job->expires_at}}"  style="color: red" type="text" name="expires_at"  class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                    
+                    </div>
+                </div>
+                <div class="card-body">
 
-                    <button class="btn btn-success btn-sm nextBtn btn-lg pull-right" type="submit">{{trans('Jobs_trans.submit')}}</button>
-                </form>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
+                    <form method="post"  action="{{ route('Applications.store') }}" autocomplete="off" enctype="multipart/form-data">
+                        @csrf
+                        <h6 style="font-family: 'Cairo', sans-serif;color: blue">{{trans('Applications_trans.Applications_information')}}</h6><br>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="hidden" name="job_id" value="{{$Job->id}}">
+                                        <input type="hidden" name="employer_id" value="{{$Job->employer_id}}">
+                                        <label>{{trans('Applications_trans.cover_letter')}} : <span class="text-danger">*</span></label>
+                                        <textarea name="cover_letter" class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>{{trans('Applications_trans.attachements')}} : <span class="text-danger">*</span></label>
+                                        <br><br>
+                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                        <button class="btn btn-success btn-block nextBtn btn-lg pull-right" type="submit">{{trans('Applications_trans.submit')}}</button>
+                    </form>
+                    <br>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<!-- row closed -->
+
 @endsection
 @section('js')
     @toastr_js
