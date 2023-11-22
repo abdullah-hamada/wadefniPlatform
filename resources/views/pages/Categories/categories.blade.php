a9e4477c482dc456fb7eb0a45ce60968b6e30ad0
Abdullah Hamada <ab989b25@gmail.com>
2023-11-22 14:09:35 +0300

    refactor categories page

--- resources/views/pages/Categories/categories.blade.php
+++ resources/views/pages/Categories/categories.blade.php
@@ -2,7 +2,7 @@
 @section('css')
     @toastr_css
 @section('title')
-{{ trans('main_trans.Main_title') }} | {{ trans('categories_trans.title_page') }}
+    {{ trans('categories_trans.title_page') }}
 @stop
 @endsection
 @section('page-header')
@@ -36,9 +36,10 @@
                     </ul>
                 </div>
             @endif
-            
-            <a href="{{route('categories.create')}}" class="btn btn-success btn-sm" role="button"
-                                   aria-pressed="true">{{trans('categories_trans.add_category')}}</a>
+
+            <button type="button" class="button x-small" data-toggle="modal" data-target="#exampleModal">
+                {{ trans('categories_trans.add_category') }}
+            </button>
             <br><br>
 
             <div class="table-responsive">
@@ -49,38 +50,205 @@
                             <th>#</th>
                             <th>{{ trans('categories_trans.name') }}</th>
                             <th>{{ trans('categories_trans.code') }}</th>
-                            <th>{{ trans('categories_trans.description') }}</th>
                             <th>{{ trans('categories_trans.status') }}</th>
                             <th>{{ trans('categories_trans.processes') }}</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php $i = 0; ?>
-                        @foreach ($categories as $category) 
+                        @foreach ($categories as $category)
                             <tr>
                                 <?php $i++; ?>
                                 <td>{{ $i }}</td>
                                 <td>{{ $category->name }}</td>
                                 <td>{{ $category->code }}</td>
-                                <td>{{ $category->description }}</td>
-                                <td>{{ $category->status }}</td>
+                                @if ($category->status == 'active')
+                                <td>{{ $category->status }} &nbsp;<i style="color:green" class="far fa-check-circle" aria-hidden="true"></i></td>
+                                @else
+                                <td>{{ $category->status }} &nbsp;<i style="color: red" class="far fa-stop-circle" aria-hidden="true"></i></td>
+                                @endif
                                 <td>
-                                    <div class="dropdown show">
-                                        <a class="btn btn-success btn-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            العمليات
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                            <a class="dropdown-item" href="{{route('categories.show',$category->id)}}"><i style="color: #ffc107" class="far fa-eye "></i>&nbsp; {{ trans('categories_trans.Show') }}</a>
-                                            <a class="dropdown-item" href="{{route('categories.edit',$category->id)}}"><i style="color:green" class="fa fa-edit"></i>&nbsp; {{ trans('categories_trans.Edit') }}</a>
-                                            <a class="dropdown-item" data-target="#delete{{ $category->id }}" data-toggle="modal"><i style="color: red" class="fa fa-trash"></i>&nbsp;  {{ trans('categories_trans.Delete') }}</a>
-                                        </div>
-                                    </div>
+                                    <button type="button" class="btn btn-info btn-sm" data-toggle="modal"
+                                        data-target="#edit{{ $category->id }}"
+                                        title="{{ trans('categories_trans.Edit') }}"><i class="fa fa-edit"></i></button>
+                                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal"
+                                        data-target="#delete{{ $category->id }}"
+                                        title="{{ trans('categories_trans.Delete') }}"><i
+                                            class="fa fa-trash"></i></button>
                                 </td>
                             </tr>
-                            @include('pages.categories.delete_category')
+
+                            <!-- edit_modal_category -->
+                            <div class="modal fade" id="edit{{ $category->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title"
+                                                id="exampleModalLabel">
+                                                {{ trans('categories_trans.edit_category') }}
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- add_form -->
+                                            <form action="{{ route('categories.update', 'test') }}" method="post">
+                                                {{ method_field('patch') }}
+                                                @csrf
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <label for="name"
+                                                            class="mr-sm-2">{{ trans('categories_trans.name') }}
+                                                            :</label>
+                                                        <input id="name" type="text" name="name"
+                                                            class="form-control"
+                                                            value="{{ $category->name }}"
+                                                            required>
+                                                        <input id="id" type="hidden" name="id" class="form-control"
+                                                            value="{{ $category->id }}">
+                                                    </div>
+                                                    <div class="col">
+                                                        <label for="code"
+                                                            class="mr-sm-2">{{ trans('categories_trans.code') }}
+                                                            :</label>
+                                                        <input type="text" class="form-control"
+                                                            value="{{ $category->code }}"
+                                                            name="code" required>
+                                                    </div>
+                                                </div>
+                                                <br>
+                                                <div class="col">
+                                                    <label>{{trans('categories_trans.status')}} : <span class="text-danger">*</span></label>
+                                                    <select name="status" value="{{ $category->status }}" class="custom-select">
+                                                        <option value="#" disabled>الرجاء الاختيار</option>
+                                                        <option value="active">{{ trans('categories_trans.active') }}</option>
+                                                        <option value="inactive">{{ trans('categories_trans.inactive') }}</option>
+                                                    </select>
+                                                </div>
+                                                <br>
+                                                <div class="form-group">
+                                                    <label
+                                                        for="exampleFormControlTextarea1">{{ trans('categories_trans.description') }}
+                                                        :</label>
+                                                    <textarea class="form-control" name="description"
+                                                        id="exampleFormControlTextarea1"
+                                                        rows="3">{{ $category->description }}</textarea>
+                                                </div>
+                                                <br><br>
+
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">{{ trans('categories_trans.Close') }}</button>
+                                                    <button type="submit"
+                                                        class="btn btn-success">{{ trans('categories_trans.submit') }}</button>
+                                                </div>
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- delete_modal_category -->
+                            <div class="modal fade" id="delete{{ $category->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title"
+                                                id="exampleModalLabel">
+                                                {{ trans('categories_trans.delete_category') }}
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{ route('categories.destroy', 'test') }}" method="post">
+                                                {{ method_field('Delete') }}
+                                                @csrf
+                                                {{ trans('categories_trans.Warning_category') }}
+                                                <input id="id" type="hidden" name="id" class="form-control"
+                                                    value="{{ $category->id }}">
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-dismiss="modal">{{ trans('categories_trans.Close') }}</button>
+                                                    <button type="submit"
+                                                        class="btn btn-danger">{{ trans('categories_trans.submit') }}</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         @endforeach
                 </table>
             </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- add_modal_category -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 style="font-family: 'Cairo', sans-serif;" class="modal-title" id="exampleModalLabel">
+                    {{ trans('categories_trans.add_category') }}
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- add_form -->
+                <form action="{{ route('categories.store') }}" method="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="col">
+                            <label for="name" class="mr-sm-2">{{ trans('categories_trans.name') }}
+                                :</label>
+                            <input id="name" type="text" name="name" class="form-control">
+                        </div>
+                        <div class="col">
+                            <label for="code" class="mr-sm-2">{{ trans('categories_trans.code') }}
+                                :</label>
+                            <input type="text" class="form-control" name="code">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col">
+                        <label>{{trans('categories_trans.status')}} : <span class="text-danger">*</span></label>
+                        <select name="status" class="custom-select">
+                            <option value="#" selected disabled>{{ trans('categories_trans.please_select') }}</option>
+                            <option value="active">{{ trans('categories_trans.active') }}</option>
+                            <option value="inactive">{{ trans('categories_trans.inactive') }}</option>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">{{ trans('categories_trans.description') }}
+                            :</label>
+                        <textarea class="form-control" name="description" id="exampleFormControlTextarea1"
+                            rows="3"></textarea>
+                    </div>
+                    <br><br>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary"
+                    data-dismiss="modal">{{ trans('categories_trans.Close') }}</button>
+                <button type="submit" class="btn btn-success">{{ trans('categories_trans.submit') }}</button>
+            </div>
+            </form>
+
         </div>
     </div>
 </div>
