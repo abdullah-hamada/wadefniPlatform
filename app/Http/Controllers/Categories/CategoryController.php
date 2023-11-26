9dfa90a4345dfae9000002b585f52b73748e9f39
Abdullah Hamada <ab989b25@gmail.com>
2023-11-26 19:40:00 +0300

    create update function

--- app/Http/Controllers/Categories/CategoryController.php
+++ app/Http/Controllers/Categories/CategoryController.php
@@ -21,15 +21,6 @@ class CategoryController extends Controller
   
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -59,27 +50,6 @@ class CategoryController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -88,9 +58,25 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+
+            $category = Category::findorfail($id);
+            $category->name = $request->name;
+            $category->code = $request->code;
+            $category->status = $request->status;
+            $category->description = $request->description;
+
+            $category->save();
+      
+            $category->save();
+
+            toastr()->success(trans('messages.Update'));
+            return redirect()->route('categories.index');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
     }
 
     /**
@@ -99,8 +85,10 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Category $category)
+    public function destroy(Request $request)
     {
-        //
+        Category::findOrFail($request->id)->delete();
+        
+        return redirect()->route('categories.index');
     }
 }
