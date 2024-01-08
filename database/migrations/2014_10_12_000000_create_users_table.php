6155671e2032bfc871b0afb60ab5e173704e8089
Abdullah Hamada <ab989b25@gmail.com>
2024-01-08 10:26:16 +0300

    add admin to enum

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('user_type', ['employer', 'job_seeker']);
+            $table->enum('user_type', ['admin','employer', 'job_seeker']);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
