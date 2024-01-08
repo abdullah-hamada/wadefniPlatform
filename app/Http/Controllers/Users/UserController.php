a07f3594ef7340e9e740f1cbf39dc0d437445bcb
Abdullah Hamada <ab989b25@gmail.com>
2024-01-08 11:50:56 +0300

    handle user  functions to check permissions

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -7,69 +7,100 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Auth\Access\AuthorizationException;
 
 class UserController extends Controller
 {
-    public function index()
+  public function index()
   {
-      $Users = User::all();
-      return view('pages.Users.users',compact('Users'));
+    try {
+        hasPermission('view users');
+        $Users = User::all();
+        return view('pages.Users.users',compact('Users'));
+    } catch (AuthorizationException $e) {
+        // User does not have the required permission
+        abort(403, $e->getMessage());
+    }
   }
 
 
   public function create()
 	{
-		return view('pages.Users.add_user');
+    try {
+      hasPermission('create users');
+
+      return view('pages.Users.add_user');
+
+    } catch (AuthorizationException $e) {
+        // User does not have the required permission
+        abort(403, $e->getMessage());
+    }
 	}
 
   public function store(Request $request)
 	{
     try {
-      $user = new User();
-      $user->name = $request->name;
-      $user->email = $request->email;
-      $user->password = Hash::make($request->password);
-      $user->save();
+        hasPermission('create users');
 
-      DB::commit(); // insert data
-      toastr()->success(trans('messages.success'));
-      return redirect()->route('Users.create');
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
 
+        DB::commit(); // insert data
+        toastr()->success(trans('messages.success'));
+        return redirect()->route('users.create');
+
+      } catch (AuthorizationException $e) {
+        // User does not have the required permission
+        return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+    }
   }
 
-  catch (\Exception $e){
-      DB::rollback();
-      return redirect()->back()->withErrors(['error' => $e->getMessage()]);
-  }	}
-
   public function edit($id)
-	{
-    $User =  User::findOrFail($id);
-    return view('pages.Users.edit_user',compact('User'));
-	}
-
-  public function update($request)
-    {
-        try {
-          // dd($request);
-            $Edit_user = User::findorfail($request);
-            $Edit_user->name = $request->name;
-            $Edit_user->email = $request->email;
-            $Edit_user->password = Hash::make($request->password);
-            $Edit_user->save();
-            toastr()->success(trans('messages.Update'));
-            return redirect()->route('Users.index');
-        } catch (\Exception $e) {
-            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
-        }
+  {
+    try {
+          hasPermission('edit users');
+          $user = User::findOrFail($id);
+          return view('pages.Users.edit_user', compact('User'));
+      } catch (AuthorizationException $e) {
+          // User does not have the required permission
+          abort(403, $e->getMessage());
     }
+  }
+
+  public function update(Request $request)
+  {
+      try {
+          hasPermission('edit users');
+          // Update the user
+          $Edit_user = User::findorfail($request->id);
+          $Edit_user->name = $request->name;
+          $Edit_user->email = $request->email;
+          $Edit_user->password = Hash::make($request->password);
+          $Edit_user->save();
+          toastr()->success(trans('messages.Update'));
+          return redirect()->route('users.index');
+      } catch (AuthorizationException $e) {
+          return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+    }
+  }
 
 	//This is the function that loads the detail page
 	public function show($id)
 	{
-    $User = User::findorfail($id);
-		return view('pages.Users.user_detail', compact('User'));
+    try {
+        hasPermission('view users');
+
+        $User = User::findorfail($id);
+
+        return view('pages.Users.user_detail', compact('User'));
+
+      } catch (AuthorizationException $e) {
+        // User does not have the required permission
+        abort(403, $e->getMessage());
+    }
 	}
 
 
@@ -81,10 +112,17 @@ class UserController extends Controller
    */
   public function destroy(Request $request, $id)
   {
+    try {
+      hasPermission('delete users');
 
-          $Users = User::findOrFail($id)->delete();
-          return redirect()->route('Users.index');
+      $User = User::findOrFail($id)->delete();
+      return redirect()->route('users.index');
+  
+    } catch (AuthorizationException $e) {
+        // User does not have the required permission
+        return redirect()->back()->withErrors(['error' => $e->getMessage()]);
       }
+  }
 
 }
 
